79230bc5cfee765a3aecb1a801d375fd62ceb36b
Javier <javiervelaz@hotmail.com>
2025-11-21 23:26:19 -0300

    a prod

--- app/dashboard/reportes/reportes/page.tsx
+++ app/dashboard/reportes/reportes/page.tsx
@@ -1,15 +1,12 @@
 'use client'
-import Operaciones from '@/app/dashboard/operaciones/admin/reportes/componentes/operaciones_diarios';
-import useAuthCheck from '@/app/lib/useAuthCheck';
 
 const Page = () => {
-  useAuthCheck();
+
   return (
     <div className="min-h-screen bg-gray-100">
       <div className="flex">
         
         <main className="min-h-screen bg-gray-100">
-        <Operaciones />
         </main>
         
       </div>
